fc3ec6eab848ee81ba667d8755dcf9fb8919e189
ALORIKA JAIN <95528398+BLACKACE13@users.noreply.github.com>
2025-06-23 21:47:34 +0530

    formatted scrape_jobs.py

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
